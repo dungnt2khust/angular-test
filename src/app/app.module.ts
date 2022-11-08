bcb77a63dae71c54c835cf7e5171adfe45fb2cd5
Nguyen Tien Dung <63250067+dungnt2khust@users.noreply.github.com>
2022-11-08 02:06:20 +0000

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-
+import { DxHtmlEditorModule } from 'devextreme-angular';
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    DxHtmlEditorModule 
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
